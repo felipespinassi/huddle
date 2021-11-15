8e4b2bbf3941e06c45b8e6331acd8febd869d6fe
Felipe Augusto <77703754+felipespinassi@users.noreply.github.com>
2021-11-15 12:12:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+ const fields = document.querySelectorAll("[required]")
+
+
+ function customValidation (event){
+     const field = event.target
+
+
+
+    //logica para verificar se existem erros
+    function verifyErrors (){
+        let foundError = false;
+
+        for(error in field.validity){
+
+            if(error != "customError" && field.validity[error]){
+                foundError = error
+            }
+        }
+
+        return foundError
+    }
+
+    const error = verifyErrors()
+
+
+
+    //trocar mensagem
+    field.setCustomValidity("Este campo é obrigatório")
+
+    }
+
+
+ for (field of fields){
+     field.addEventListener("invalid", customValidation)
+ }
+ 
+
+ document.querySelector('form')
+
+.addEventListener("submit", event =>{
+    console.log("enviar o formulario")
+
+})
